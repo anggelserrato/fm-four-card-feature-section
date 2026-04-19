7c3cebca9d9b2ac03f1513970893fe8591d89784
Angel Serrato <serrato.anggel@gmail.com>
2026-04-19 09:31:18 -0500

    feat: initialize project with react, vite, and tailwindcss

---
+++ src/components/FourCardSection.jsx
@@ -0,0 +1,26 @@
+function FourCardSection() {
+  return (
+    <main>
+      <article>
+        <p>Reliable, efficient delivery</p>
+        <p>Powered by Technology</p>
+        <p>
+          Our Artificial Intelligence powered tools use millions of project data
+          points to ensure that your project is successful
+        </p>
+        <p>Supervisor</p>
+        <p>Monitors activity to identify project roadblocks</p>
+        <p>Team Builder</p>
+        <p>
+          Scans our talent network to create the optimal team for your project
+        </p>
+        <p>Karma</p>
+        <p>Regularly evaluates our talent to ensure quality</p>
+        <p>Calculator</p>
+        <p>Uses data from past projects to provide better delivery estimates</p>
+      </article>
+    </main>
+  );
+}
+
+export default FourCardSection;
