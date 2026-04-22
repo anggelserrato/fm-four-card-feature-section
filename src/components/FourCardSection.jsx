f46b6dd7047ef96fae5cd44ed3db472783448acf
Angel Serrato <serrato.anggel@gmail.com>
2026-04-22 11:23:06 -0500

    refactor(structure): reorganize with semantic html and reusable components

--- src/components/FourCardSection.jsx
+++ src/components/FourCardSection.jsx
@@ -1,23 +1,43 @@
+import supervisorIcon from '../assets/icon-supervisor.svg'
+import teamBuilderIcon from '../assets/icon-team-builder.svg'
+import karmaIcon from '../assets/icon-karma.svg'
+import calculatorIcon from '../assets/icon-calculator.svg'
+
 function FourCardSection() {
+  const cards = [
+    {
+      id: 1, icon: supervisorIcon, title: 'Supervisor', description: 'Monitors activity to identify project roadblocks'
+    },
+    {
+      id: 2, icon: teamBuilderIcon, title: 'Team Builder', description: 'Scans our talent network to create the optimal team for your project'
+    },
+    {
+      id: 3, icon: karmaIcon, title: 'Karma', description: 'Regularly evaluates our talent to ensure quality'
+    },
+    {
+      id: 4, icon: calculatorIcon, title: 'Calculator', description: 'Uses data from past projects to provide better delivery estimates'
+    }
+  ]
   return (
     <main>
       <article>
-        <p>Reliable, efficient delivery</p>
-        <p>Powered by Technology</p>
-        <p>
-          Our Artificial Intelligence powered tools use millions of project data
-          points to ensure that your project is successful
-        </p>
-        <p>Supervisor</p>
-        <p>Monitors activity to identify project roadblocks</p>
-        <p>Team Builder</p>
-        <p>
-          Scans our talent network to create the optimal team for your project
-        </p>
-        <p>Karma</p>
-        <p>Regularly evaluates our talent to ensure quality</p>
-        <p>Calculator</p>
-        <p>Uses data from past projects to provide better delivery estimates</p>
+        <header>
+          <h1>Reliable, efficient delivery</h1>
+          <p>Powered by Technology</p>
+          <p>
+            Our Artificial Intelligence powered tools use millions of project data
+            points to ensure that your project is successful
+          </p>
+        </header>
+        <section>
+          {cards.map((card) => (
+            <div key={card.id}>
+              <h2>{card.title}</h2>
+              <p>{card.description}</p>
+              <img src={card.icon} alt={card.title} />
+            </div>
+          ))}
+        </section>
       </article>
     </main>
   );
